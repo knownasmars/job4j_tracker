83028b8cc2f164af4546e6bbeff4c8e081664e1c
Mars <knownasmars@gmail.com>
2022-06-13 20:54:15 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,37 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+
+    private String surname;
+
+    private String group;
+
+    private Date admissionDate;
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getAdmissionDate() {
+        return admissionDate;
+    }
+
+    public void setAdmissionDate(Date admissionDate) {
+        this.admissionDate = admissionDate;
+    }
+}
