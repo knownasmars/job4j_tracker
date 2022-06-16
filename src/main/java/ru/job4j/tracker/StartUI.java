65d1454c03e3bdcfcd908eebc58e646a292baa3f
Mars <knownasmars@gmail.com>
2022-06-16 19:59:33 +0300

    9. toString [#310018]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,10 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item item = new Item(1, "empty");
+        System.out.println(item);
+    }
+}
