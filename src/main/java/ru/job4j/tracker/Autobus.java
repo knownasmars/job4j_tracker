2d6e1448a9ef18d4a12ec4492aa81057b4bf468a
Mars <knownasmars@gmail.com>
2022-06-18 01:36:18 +0300

    3.2. Зачем нужно приведение типов [#310019]

--- src/main/java/ru/job4j/tracker/Autobus.java
+++ src/main/java/ru/job4j/tracker/Autobus.java
@@ -1,6 +1,6 @@
 package ru.job4j.tracker;
 
-public class Autobus implements Vehicle{
+public class Autobus implements Vehicle {
     @Override
     public void move() {
         System.out.println(getClass().getSimpleName() + " развожу детей по школам.");
