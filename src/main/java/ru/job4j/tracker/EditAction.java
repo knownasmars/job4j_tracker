7c7d0d061eaec44481a18ccb9598333b547031a6
Mars <knownasmars@gmail.com>
2022-06-19 21:42:01 +0300

    8. Реализация меню за счет шаблона стратегия. [#181780 #314210]

--- src/main/java/ru/job4j/tracker/EditAction.java
+++ src/main/java/ru/job4j/tracker/EditAction.java
@@ -9,7 +9,7 @@ public class EditAction implements UserAction {
     @Override
     public boolean execute(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
-        int id = input.askInt("Enter id: " + System.lineSeparator());
+        int id = input.askInt("Enter id: ");
         String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
