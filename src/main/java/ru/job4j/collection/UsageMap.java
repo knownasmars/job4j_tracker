7d45ec4341da6016b453d823bc09d13d15cd975b
Mars <knownasmars@gmail.com>
2022-08-02 22:05:16 +0300

    0. Map, HashMap [#212652 #327190]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,6 +6,9 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("knownasmars@gmail.com", "Suleimanov Mars Eduardovich");
+        map.put("known123asmars@gmail.com", "Ivanov Mars Eduardovich");
+        map.put("kn53523ownasmars@gmail.com", "Suleimanov Dima Eduardovich");
+        map.put("knownasmars@gmail.com", "Suleimanov Mars Eduardovich");
         for (String k : map.keySet()) {
             System.out.println(k + " - " + map.get(k));
         }
