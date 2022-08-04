00db63c8f826ad4e1a084e3e71f8368db5d3e180
Mars <knownasmars@gmail.com>
2022-08-04 20:16:49 +0300

    2. Что такое JavaDoc [#313879 #328960]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -22,7 +22,7 @@ public class Tracker {
     public List<Item> findByName(String key) {
         List<Item> rsl = new ArrayList<>();
         for (Item item : items) {
-            if (key.equals(item)) {
+            if (key.equals(item.getName())) {
                 rsl.add(item);
             }
         }
