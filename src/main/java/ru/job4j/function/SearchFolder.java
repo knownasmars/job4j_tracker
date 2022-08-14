d1a57da441fed566582e65e40d85dbbedef4317e
Mars <knownasmars@gmail.com>
2022-08-15 01:15:44 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209 #332367]

--- src/main/java/ru/job4j/function/SearchFolder.java
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -11,7 +11,7 @@ public class SearchFolder {
         for (Folder f : list) {
             if (pred.test(f)) {
                 rsl.add(f);
-            };
+            }
         }
         return rsl;
     }
