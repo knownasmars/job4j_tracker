357f292c27647f4c669cec963fe7a308c894c214
Mars <knownasmars@gmail.com>
2022-08-16 15:46:21 +0300

    1. Фильтрация учеников [#110058 #332739]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -37,7 +37,7 @@ public class SchoolTest {
                 new Student(80, "Surname8")
         );
         School sc = new School();
-        Predicate<Student> pr = s -> s.getScore() >= 50 && s.getScore() <= 70;
+        Predicate<Student> pr = s -> s.getScore() >= 50 && s.getScore() < 70;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));
@@ -55,7 +55,7 @@ public class SchoolTest {
                 new Student(90, "Surname9")
         );
         School sc = new School();
-        Predicate<Student> pr = s -> s.getScore() >= 0 && s.getScore() <= 50;
+        Predicate<Student> pr = s -> s.getScore() >= 0 && s.getScore() < 50;
         List<Student> rsl = sc.collect(students, pr);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(10, "Surname1"));
