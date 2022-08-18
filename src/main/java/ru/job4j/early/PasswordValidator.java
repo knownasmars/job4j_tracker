eb1a6bfed7b410f8021e921a53d7801ce30c00c6
Mars <knownasmars@gmail.com>
2022-08-18 17:19:52 +0300

    4. Принципы раннего возврата и охранных выражений [#504875]

---
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -0,0 +1,54 @@
+package ru.job4j.early;
+
+public class PasswordValidator {
+    public static String validate(String password) {
+        if (password == null) {
+            throw new IllegalArgumentException("The password is null");
+        }
+        return "Password is Valid";
+    }
+
+    public static void lengthVal (String password) {
+        if (password.length() < 8 || password.length() > 32) {
+            throw new IllegalArgumentException("Password's length is out of range");
+        }
+    }
+
+    public static void substringVal (String password) {
+        if (password.equals("12345") || password.equals("qwerty")
+                || password.equals("password") || password.equals("admin")
+                || password.equals("user")) {
+            throw new IllegalArgumentException("Please, choose another password");
+        }
+    }
+
+    public static void digitVal (String password) {
+        int digits = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (Character.isDigit(password.charAt(i))) {
+                digits++;
+            }
+        }
+        if (digits < 1) {
+            throw new IllegalArgumentException("No digits in password. Try again please");
+        }
+    }
+
+    public static void symbolVal (String password) {
+        int symbols = 0;
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetterOrDigit(password.charAt(i))) {
+                symbols++;
+            }
+        }
+        if (symbols < 1) {
+            throw new IllegalArgumentException("No symbols in password. Try again please");
+        }
+    }
+
+    public static void upperLowerVal (String password) {
+        if (password.equals(password.toUpperCase()) || password.equals(password.toLowerCase())) {
+            throw new IllegalArgumentException("Please don't choose only UpperCase or LowerCase");
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/early/PasswordValidatorTest.java
@@ -0,0 +1,75 @@
+package ru.job4j.early;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static ru.job4j.early.PasswordValidator.*;
+
+public class PasswordValidatorTest {
+
+    @Test
+    public void whenPasswordIsNull() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    validate(null);
+                });
+        Assertions.assertEquals(
+                "The password is null",
+                thrown.getMessage());
+    }
+
+    @Test
+    public void whenPasswordHasWrongLength() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    lengthVal("123222222");
+                });
+        Assertions.assertEquals(
+                "Password's length is out of range",
+                thrown.getMessage());
+    }
+
+    @Test
+    public void whenPasswordIsQwerty() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    substringVal("qwerty");
+                });
+        Assertions.assertEquals(
+                "Please, choose another password",
+                thrown.getMessage());
+    }
+
+    @Test
+    public void whenPasswordHasNoDigis() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    digitVal("abcdefghi");
+                });
+        Assertions.assertEquals(
+                "No digits in password. Try again please",
+                thrown.getMessage());
+    }
+
+    @Test
+    public void whenPasswordHasNoLetterNoDigits() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    symbolVal("123456abcdef");
+                });
+        Assertions.assertEquals(
+                "No symbols in password. Try again please",
+                thrown.getMessage());
+    }
+
+    @Test
+    public void whenPasswordUpperVal() {
+        IllegalArgumentException thrown = Assertions.assertThrows(
+                IllegalArgumentException.class, () -> {
+                    upperLowerVal("AAAAA");
+                });
+        Assertions.assertEquals(
+                "Please don't choose only UpperCase or LowerCase",
+                thrown.getMessage());
+    }
+}
