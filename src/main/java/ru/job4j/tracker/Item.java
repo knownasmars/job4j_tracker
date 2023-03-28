bd51f8fb525cbd0515b44bae8a66aa7dc5564f07
Mars <knownasmars@gmail.com>
2023-03-28 17:20:34 +0300

    1. Lombok. Настройка IDEA [#504888]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,16 +2,20 @@ package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
+import lombok.*;
+
+@Data
+@EqualsAndHashCode
 public class Item implements Comparable<Item> {
 
     private int id;
 
     private String name;
 
+    @EqualsAndHashCode.Exclude
     private LocalDateTime created = LocalDateTime.now();
-
+    @EqualsAndHashCode.Exclude
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy");
 
     public Item() {
@@ -30,55 +34,6 @@ public class Item implements Comparable<Item> {
         this.id = id;
         this.name = name;
         this.created = created;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" + "id=" + id
-                + ", name='" + name + '\''
-                + ", created="
-                + created.format(FORMATTER) + '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Item item = (Item) o;
-        return id == item.id
-                && Objects.equals(name, item.name)
-                && Objects.equals(
-                        created.minusNanos(created.getNano()),
-                        item.created.minusNanos(item.created.getNano()));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name);
     }
 
     @Override
